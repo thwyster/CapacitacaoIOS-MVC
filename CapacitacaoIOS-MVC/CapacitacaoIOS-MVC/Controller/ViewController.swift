0c58eb8ef234a32a356442ae4193a562cfba50df
ALUNO <aluno@LADMAC0129.local>
2019-03-01 20:54:59 -0300

    Primeiro Commit

---
+++ CapacitacaoIOS-MVC/CapacitacaoIOS-MVC/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapacitacaoIOS-MVC
+//
+//  Created by ALUNO on 01/03/19.
+//  Copyright © 2019 ALUNO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CapacitacaoIOS-MVC/CapacitacaoIOS-MVC/Model/Filmes.swift
@@ -0,0 +1,58 @@
+//
+//  Filmes.swift
+//  CapacitacaoIOS-MVC
+//
+//  Created by ALUNO on 01/03/19.
+//  Copyright © 2019 ALUNO. All rights reserved.
+//
+
+import Foundation
+
+class Filmes {
+    var listaFilmes = [String]()
+    
+    init() {
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Bleach")
+        listaFilmes.append("Jogador Numero 1")
+        listaFilmes.append("Star Wars")
+        listaFilmes.append("Vingadores")
+        listaFilmes.append("Matrix")
+    }
+}
